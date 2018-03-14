5c1dfa7e0c937656a9043f4b203b2b349b226e54
Minuddin Ahmed Rana <minuddinrana@gmail.com>
2018-03-14 15:54:45 +0600

    Add postgres to docker

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,3 @@ RUN mkdir /code
 WORKDIR /code
 COPY . /code/
 RUN pip install -r requirements.txt
-
-
--- demoapi/settings.py
+++ demoapi/settings.py
@@ -62,22 +62,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'demoapi.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': env('DATABASE_NAME'),
-#         'USER': env('DATABASE_USER'),
-#         'PASSWORD': env('DATABASE_PASSWORD'),
-#         'HOST': env('DATABASE_HOST'),
-#         'PORT': env('DATABASE_PORT'),
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'the_database'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'the_database'),
+#     }
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
